12658b1a758e1c8cbf90e86fea715e9b75422521
diegojsd <diego_duarte@id.uff.br>
2019-05-17 13:42:17 -0300

    acerto no codigo

--- modulo_02/16_heranca.rb
+++ modulo_02/16_heranca.rb
@@ -17,13 +17,16 @@ class PessoaJuridica < Pessoa
 end
 
 p1 = PessoaFisica.new
-p2 = PessoaJuridica.new
 
 p1.email = "diego_duarte@id.uff.br"
 p1.cpf = 12345678900
+p1.nome = "Diego"
+
+p2 = PessoaJuridica.new
+
 p2.nome = "Jurandir"
 p2.cnpj = 1234567890001
-p1.nome = "Diego"
+
 
 puts p1.falar
 puts p2.pagar_fornecedor
