812efac1cc8c8532fdd7d74426a657fedb68044b
diegojsd <diego_duarte@id.uff.br>
2019-05-22 23:20:41 -0300

    novas gems e testes cpf,cep,lerolero

--- modulo_02/modulos/classes/app.rb
+++ modulo_02/modulos/classes/app.rb
@@ -1,6 +1,30 @@
 require_relative 'pagamento'
+require 'documentos_br'
+require 'tty-spinner'
+require 'lerolero_generator'
+require 'correios-cep'
 
 include Pagamento
+include DocumentosBr
+include TTY
+include LeroleroGenerator
+include Correios
+
+
 
 p = Pagamento::Visa.new
 puts p.pagando
+
+cpf = DocumentosBr.cpf_formatado
+
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu cep: "
+cep = gets.chomp
+endereco = Correios::CEP::AddressFinder.get(cep)
+
+
+c1 = Pagamento::Cliente.new(nome, cpf, endereco[:city])
+puts c1.presents
+puts c1.opniao(LeroleroGenerator.sentence)
+
--- modulo_02/modulos/classes/pagamento.rb
+++ modulo_02/modulos/classes/pagamento.rb
@@ -1,7 +1,21 @@
 module Pagamento
   class Visa
     def pagando
-      "Pagando com VISA"
+      "Pagando com Visa !!"
+    end
+  end
+  class Cliente
+    attr_accessor :nome, :cpf, :cidade
+    def initialize(nome = "Hermanoteu", cpf = 123, cidade = "Pentescopeia")
+      @nome = nome
+      @cpf = cpf
+      @cidade = cidade
+    end
+    def presents
+      "Meu nome é #{self.nome}, meu cpf é #{self.cpf}, eu moro em #{self.cidade}"
+    end
+    def opniao(op)
+      "Eu acho que #{op}"
     end
   end
 end
